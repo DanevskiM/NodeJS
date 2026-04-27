e7fb9365d9a03b3c081e6378e65a2337b2800539
DanevskiM <danevskimetodija5@gmail.com>
2026-04-27 23:51:40 +0200

    creating-first-server

--- JavaScriptQuickRefresher/play.js
+++ JavaScriptQuickRefresher/play.js
@@ -1,12 +1,22 @@
-const person = {
-    name: 'Max',
-    age: 29,
-    greet() {
-        console.log('Hey, I am ' + this.name);
-    },
-};
+// const person = {
+//     name: 'Max',
+//     age: 29,
+//     greet() {
+//         console.log('Hey, I am ' + this.name);
+//     },
+// };
+//
+// const printName = ({name}) => {console.log(name)}
+//
+// printName(person);
+//
+// const { name, age } = person;
+// console.log(name, age);
+//
+// const hobbies = ['Sports', 'Cooking'];
+// const [hobby1, hobby2] = hobbies;
+// console.log(hobby1, hobby2);
 
-const printName = (person) => {}
 // person.greet()
 // const coppiedPerson = {...person};
 // console.log(coppiedPerson);
@@ -26,3 +36,26 @@ const printName = (person) => {}
 //     return args;
 // }
 // console.log(toArray(1, 2, 3, 4, 5));
+
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!');
+        }, 1500);
+    });
+    return promise;
+};
+
+setTimeout(() => {
+    console.log('Timer is done!');
+    fetchData()
+        .then(text => {
+            console.log(text);
+            return fetchData();
+        }).then(text2 => {
+        console.log(text2);
+    });
+}, 2000)
+
+console.log('Hello');
+console.log('Hi');
