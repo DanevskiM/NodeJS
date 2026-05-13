b5bf6de86948c17431f03f1ed2731cea9dd97588
DanevskiM <danevskimetodija5@gmail.com>
2026-05-13 22:12:21 +0200

    product-data

--- first-expressJS/controllers/shop.js
+++ first-expressJS/controllers/shop.js
@@ -25,7 +25,13 @@ exports.getCart = (req, res, next) => {
         path: '/cart',
         pageTitle: 'Your Cart'
     });
+}
 
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders'
+    });
 }
 
 exports.getCheckout = (req, res, next) => {
--- first-expressJS/routes/shop.js
+++ first-expressJS/routes/shop.js
@@ -4,13 +4,16 @@ const express = require('express');
 
 const shopController = require('../controllers/shop');
 
+
 const router = express.Router();
 
-router.get('/', productsController.getIndex);
+router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
 router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
 
