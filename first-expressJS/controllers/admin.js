c623ee819d64596d79ecec69558bab1a492b2ee8
DanevskiM <danevskimetodija5@gmail.com>
2026-05-17 23:58:36 +0200

    functional-deleting

--- first-expressJS/controllers/admin.js
+++ first-expressJS/controllers/admin.js
@@ -43,8 +43,14 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
-    const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice);
+    const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedPrice, updatedDesc);
     updatedProduct.save();
+    res.redirect('/admin/products');
+}
+
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.deleteById(prodId);
     res.redirect('/admin/products');
 }
 
